ea82fcdcde9883707f3cc9bc6b3aa1e67db1b633
Charlie 🐕 <charlie@charlieverse.ai>
2026-06-12 08:34:40 -0400

    Fix Fable, Add Config

--- emily/hooks/check-context.sh
+++ emily/hooks/check-context.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+check() {
+    percent="$1"
+    if (( percent < 10 )); then
+        percent=10
+    elif (( percent > 100)); then
+        percent=100
+    fi
+
+    echo "$percent"
+}
 
 input=$(cat)
 transcript=$(echo "$input" | jq -r ".transcript_path")
@@ -22,9 +32,14 @@ read -r model token_usage < <(jq -rs '
 # Default 200k
 max_context=200000
 
-# Opus and 1m models, not super accurate but close enough
-if [[ "$model" == *"[1m]"* || "$model" == *"opus"* ]]; then
+critical_threshold=$(check "${CLAUDE_PLUGIN_OPTION_CRITICAL_THRESHOLD_200K-40}")
+low_threshold=$(check "${CLAUDE_PLUGIN_OPTION_LOW_THRESHOLD_200K-60}")
+
+# Opus/Fable and 1m models, not super accurate but close enough
+if [[ "$model" == *"[1m]"* || "$model" == *"opus"* || "$model" == *"fable"* ]]; then
     max_context=1000000
+    critical_threshold=$(check "${CLAUDE_PLUGIN_OPTION_CRITICAL_THRESHOLD_1M-40}")
+    low_threshold=$(check "${CLAUDE_PLUGIN_OPTION_LOW_THRESHOLD_1M-60}")
 fi
 
 remaining=$(awk "BEGIN { printf \"%.2f\", (${max_context} - ${token_usage}) }")
@@ -32,9 +47,9 @@ remaining_percent=$(awk "BEGIN { printf \"%.2f\", (${remaining} / ${max_context}
 
 remaining_int=${remaining_percent%.*}
 
-if (( remaining_int < 40 )); then
+if (( remaining_int < critical_threshold )); then
     echo "Remaining context is CRITICALLY LOW! You NEED to finish any in-flight work then insist on starting a new session before continuing"
-elif (( remaining_int < 60 )); then
+elif (( remaining_int < low_threshold )); then
     echo "Remaining context is LOW! It's time to start wrapping up the session! Finish up in-flight work, save session, and suggest starting a new session before starting any new work."
 fi
 
